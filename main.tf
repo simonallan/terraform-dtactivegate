8bf04884edf55ad0c7189fc370125d7debdfdb53
simonallan <simon.allan@cancer.org.uk>
2020-10-08 22:04:54 +0100

    configures single ec2

--- main.tf
+++ main.tf
@@ -4,10 +4,49 @@ provider "aws" {
   profile                 = "terra-dev"
 }
 
-resource "aws_instance" "activegate" {
+variable "product_name" {
+  description = "Name of application being deployed"
+  type        = string
+  default     = "dtactivegate"
+}
+
+variable "environment" {
+  description = "Describes business criticality of this deployment"
+  type        = string
+  default     = "dev"
+}
+
+variable "instance_count" {
+  description = "An index to count instances"
+  type        = number
+  default     = "1"
+}
+
+variable "dynatrace_ports" {
+  description = "Default Ports used for Dynatrace traffic"
+  type        = map
+  default = {
+    oneagent   = 9999
+    activegate = 443
+  }
+}
+
+locals {
+  # Common tags to be assigned to all resources
+  common_tags = {
+    product     = var.product_name
+    environment = var.environment
+    owner       = "websysadmins"
+    contact     = "websysadmins@cancer.org.uk"
+  }
+  deployment_name = "${var.product_name}-${var.environment}"
+}
+
+resource "aws_instance" "dtactivegate_instance" {
+  count                  = var.instance_count
   ami                    = "ami-0823c236601fef765"
   instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.allow_oneagent.id]
+  vpc_security_group_ids = [aws_security_group.allow_dynatrace_traffic_inbound.id]
 
   user_data = <<-EOF
               #!/bin/bash
@@ -15,21 +54,31 @@ resource "aws_instance" "activegate" {
               nohup busybox httpd -f -p 9999 &
               EOF
 
-  tags = {
-    Name = "activegate"
-  }
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "${local.deployment_name}-${count.index + 1}"
+    }
+  )
 }
 
-resource "aws_security_group" "allow_oneagent" {
-  name        = "allow_oneagent"
+resource "aws_security_group" "allow_dynatrace_traffic_inbound" {
+  name        = "allow_dynatrace_traffic_inbound"
   description = "Allow OneAgent traffic inbound"
 
   ingress {
-    from_port   = 9999
-    to_port     = 9999
+    from_port   = var.dynatrace_ports.oneagent
+    to_port     = var.dynatrace_ports.oneagent
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  ingress {
+    from_port   = var.dynatrace_ports.activegate
+    to_port     = var.dynatrace_ports.activegate
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = local.common_tags
 }
-
-
