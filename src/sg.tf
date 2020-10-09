5818511f7a659cfe81ddbea6405d1e53a50342fe
smith193 <paul.smtih@cancer.org>
2020-10-09 14:57:35 +0100

    added self to rule

--- src/sg.tf
+++ src/sg.tf
@@ -15,6 +15,20 @@ resource "aws_security_group" "allow_dynatrace_traffic_inbound" {
     cidr_blocks = var.dynatrace_CIDR-ext-in
   }
 
+  ingress {
+    from_port   = var.dynatrace_ports.activegate
+    to_port     = var.dynatrace_ports.activegate
+    protocol    = var.dynatrace_protocol
+    self = true
+  }
+
+  ingress {
+    from_port   = var.dynatrace_ports.oneagent
+    to_port     = var.dynatrace_ports.oneagent
+    protocol    = var.dynatrace_protocol
+    self = true
+  }
+
   tags = merge(
     local.common_tags,
     {
