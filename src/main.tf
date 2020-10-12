81a4dfb7c663a86b8f70708734718e799c29c6b5
smith193 <paul.smtih@cancer.org>
2020-10-12 12:53:21 +0100

    remvoed plan from repo and added gitignore for this, also version of AWS added

--- src/main.tf
+++ src/main.tf
@@ -2,6 +2,7 @@ provider "aws" {
   region                  = var.region
   shared_credentials_file = var.shared_credentials_file
   profile                 = var.profile
+  version                  = "~> 2.5"
 }
 
 locals {
